3ddaaee7c8c81c6b3c4c387e2e320e70da9cb7c4
Martti <marttisyber@gmail.com>
2022-03-06 15:03:06 +0300

    queue verified email

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+// Notification
+use App\Notifications\SendVerifyWithQueueNotification;
+
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
@@ -56,4 +59,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new SendVerifyWithQueueNotification());
+    }
 }
