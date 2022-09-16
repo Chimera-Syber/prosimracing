8d09422df1d59020aaac03649a857ea68fccc616
Chimera-Syber <marttisyber@gmail.com>
2022-09-16 13:09:58 +0300

    yandex counter

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -17,6 +17,24 @@
     <!-- Yandex WebMaster --> 
     <meta name="yandex-verification" content="b675b53129b4121c" />
 
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(90432963, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/90432963" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
     <!-- OG Links -->
     <meta property="og:title" content="@yield('og_title')">
     <meta property="og:description" content="@yield('og_description')">
